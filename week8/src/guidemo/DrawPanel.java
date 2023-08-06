d2e1b077b8517b5c8fc9c275d0b1e9fe6e1117da
Akivalpha <74569984+Akivalpha@users.noreply.github.com>
2023-08-07 00:25:21 +0300

    Add files via upload

---
+++ week8/src/guidemo/DrawPanel.java
@@ -0,0 +1,161 @@
+package guidemo;
+
+import java.applet.AudioClip;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class DrawPanel extends JPanel {
+
+    private TextItem text = new TextItem();
+    private Image backgroundImage = null;
+    private Color borderColor = Color.DARK_GRAY;
+    private int borderThickness = 3;
+    private Color gradientOverlayColor = Color.WHITE;
+    private boolean horizontalOverlay = false;
+    private BufferedImage currentDrawImage;
+
+    private ArrayList<ImageItem> images = new ArrayList<ImageItem>();
+
+    public DrawPanel() {
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.DARK_GRAY);
+        setBorder(BorderFactory.createLineBorder(borderColor, borderThickness));
+        setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        addMouseListener(new MouseAdapter() {
+            AudioClip clink = Util.getSound("clink.wav");
+            AudioClip lase = Util.getSound("lase.wav");
+
+            public void mousePressed(MouseEvent evt) {
+                int x = evt.getX();
+                int y = evt.getY();
+                if (currentDrawImage != null) {
+                    if (clink != null)
+                        clink.play();
+                    images.add(new ImageItem(currentDrawImage, x, y));
+                    
+                    repaint();
+                } else {
+                    for (int i = images.size() - 1; i >= 0; i--) {
+                        if (images.get(i).contains(x, y)) {
+                            if (lase != null)
+                                lase.play();
+                            images.remove(i);
+                            repaint();
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g1) {
+        super.paintComponent(g1);
+        Graphics2D g2 = (Graphics2D) g1;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if (backgroundImage != null)
+            g2.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        if (gradientOverlayColor != null) {
+            int r = gradientOverlayColor.getRed();
+            int b = gradientOverlayColor.getBlue();
+            int g = gradientOverlayColor.getGreen();
+            Color startColor = new Color(r, g, b, 50);
+            Color endColor = new Color(r, g, b, 200);
+            if (horizontalOverlay)
+                g2.setPaint(new GradientPaint(0, 0, startColor, getWidth(), 0, endColor, false));
+            else
+                g2.setPaint(new GradientPaint(0, 0, startColor, 0, getHeight(), endColor, false));
+
+            g2.fillRect(0, 0, getWidth(), getHeight());
+        }
+        text.draw(g2, getWidth() / 2, getHeight() / 2);
+        for (ImageItem img : images)
+            img.draw(g2);
+    }
+
+    // Getters and setters for properties
+
+    public Image getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+        repaint();
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+        setBorder(BorderFactory.createLineBorder(borderColor, borderThickness));
+        repaint();
+    }
+
+    public int getBorderThickness() {
+        return borderThickness;
+    }
+
+    public void setBorderThickness(int borderThickness) {
+        this.borderThickness = borderThickness;
+        setBorder(BorderFactory.createLineBorder(borderColor, borderThickness));
+        repaint();
+    }
+
+    public Color getGradientOverlayColor() {
+        return gradientOverlayColor;
+    }
+
+    public void setGradientOverlayColor(Color gradientOverlayColor) {
+        this.gradientOverlayColor = gradientOverlayColor;
+        repaint();
+    }
+
+    public boolean isHorizontalOverlay() {
+        return horizontalOverlay;
+    }
+
+    public void setHorizontalOverlay(boolean horizontalOverlay) {
+        this.horizontalOverlay = horizontalOverlay;
+        repaint();
+    }
+
+    public BufferedImage getCurrentDrawImage() {
+        return currentDrawImage;
+    }
+
+    public void setCurrentDrawImage(BufferedImage currentDrawImage) {
+        this.currentDrawImage = currentDrawImage;
+    }
+
+    public TextItem getTextItem() {
+        return text;
+    }
+
+    // Other utility methods
+
+    public BufferedImage copyImage() {
+        BufferedImage copy = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics g = copy.createGraphics();
+        paintComponent(g);
+        g.dispose();
+        return copy;
+    }
+
+    public void clear() {
+        text = new TextItem();
+        backgroundImage = null;
+        setBackground(Color.DARK_GRAY);
+        gradientOverlayColor = Color.WHITE;
+        horizontalOverlay = false;
+        borderThickness = 3;
+        setBorderColor(Color.DARK_GRAY);
+        images.clear();
+        repaint();
+    }
+}
---
+++ week8/src/guidemo/IconSupport.java
@@ -0,0 +1,100 @@
+package guidemo;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class IconSupport {
+
+    private DrawPanel panel;
+    private ArrayList<BufferedImage> iconImages = new ArrayList<BufferedImage>();
+    private ArrayList<Action> actions = new ArrayList<Action>();
+    
+
+    public IconSupport(DrawPanel owner) {
+        panel = owner;
+        // Array of icon names for the buttons
+        String[] iconNames = {"bell", "camera", "flower", "star", "check", "crossout", "tux", "bomb", "keyboard", "lightbulb", "tv"};
+        for (String name : iconNames) {
+            BufferedImage img = Util.getBufferedImageResource(name + ".png");
+            if (img != null) {
+                iconImages.add(img);
+                actions.add(new SelectIconAction(name, iconImages.size() - 1));
+            }
+        }
+        actions.add(new NoIconAction());
+    }
+
+    /**
+     * Return a toolbar containing buttons representing the images that can be added
+     * to the DrawPanel.
+     * 
+     * @param horizontal a value of JToolBar.HORIZONTAL or JToolBar.VERTICAL tells
+     *                   whether the toolbar is meant to have horizontal or vertical
+     *                   orientation.
+     */
+    public JToolBar createToolbar(boolean horizontal) {
+        JToolBar tbar = new JToolBar(horizontal ? JToolBar.HORIZONTAL : JToolBar.VERTICAL);
+        for (int i = 0; i < actions.size() - 1; i++)
+            tbar.add(actions.get(i));
+        tbar.addSeparator(new Dimension(15, 0));
+        tbar.add(actions.get(actions.size() - 1));
+        return tbar;
+    }
+
+    public JMenu createMenu() {
+        JMenu stamper = new JMenu("Stamper");
+        for (int i = 0; i < actions.size() - 1; i++)
+            stamper.add(actions.get(i));
+        stamper.addSeparator();
+        stamper.add(actions.get(actions.size() - 1));
+        return stamper;
+    }
+
+    private class NoIconAction extends AbstractAction {
+        NoIconAction() {
+            super("Eraser");
+            BufferedImage del = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+            Graphics g = del.createGraphics();
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, 32, 32);
+            g.setColor(Color.RED);
+            g.drawString("DEL", 5, 20);
+            g.dispose();
+            putValue(Action.SMALL_ICON, new ImageIcon(del));
+            putValue(Action.SHORT_DESCRIPTION, "Use Mouse to Erase Icons");
+        }
+
+        public void actionPerformed(ActionEvent evt) {
+            BufferedImage currentImage = panel.getCurrentDrawImage();
+            panel.setCurrentDrawImage(null);
+            panel.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+            if (currentImage != null) {
+                
+            }
+        }
+    }
+
+    private class SelectIconAction extends AbstractAction {
+        int iconNumber;
+
+        SelectIconAction(String name, int n) {
+            super(name, new ImageIcon(iconImages.get(n)));
+            iconNumber = n;
+            putValue(Action.SHORT_DESCRIPTION, "Use Mouse to Stamp this Icon");
+        }
+
+        public void actionPerformed(ActionEvent evt) {
+            BufferedImage image = iconImages.get(iconNumber);
+            panel.setCurrentDrawImage(image);
+            Cursor c = Util.createImageCursor(image, image.getWidth() / 2, image.getHeight() / 2);
+            panel.setCursor(c);
+             
+        }
+    }
+}
+
+
